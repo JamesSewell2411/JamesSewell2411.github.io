0ce7e31fae2deb322030fc9425e8f5813d2e0491
JamesSewell2411 <35300382+JamesSewell2411@users.noreply.github.com>
2018-01-10 15:33:04 +0000

    Canvas Change

--- js/script.js
+++ js/script.js
@@ -9,30 +9,27 @@ window.onload = function()
 	if (window.innerWidth <=400)
 	{
 		//Mobile sized screen
-		canvas.width = (window.innerWidth)-25;
-		canvas.height = canvas.width;
+		canvas.width = 380;
+		canvas.height = 380;
 	}
 	else if ((window.innerWidth > 400) && (window.innerWidth< 800))
 	{
 		//Tablet sized screen
-		canvas.width = (window.innerWidth - 38)
-		canvas.height = canvas.width;
+		canvas.width = 600
+		canvas.height = 600;
 	}
-	else if (window.innerWidth > 800)
+	else if ((window.innerWidth > 800) && (window.innerWidth < 1000))
 	{
-		//Desktop sized screen
-		canvas.width = window.innerWidth - 200;
-		if (canvas.width > 1600)
-		{
-			canvas.width = 1600;
-		}
-		canvas.height = canvas.width;
-		if (canvas.height > 1100)
-		{
-			canvas.height = 1100;
-		}
+		//Desktop (Laptop) sized screen
+		canvas.width = 800;
+		canvas.height = 800;
 	}
-
+	else if ((window.innerWidth > 1000) && )
+	{
+		//Desktop (Laptop) sized screen
+		canvas.width = 1000;
+		canvas.height = 1000;
+	}
 	my_context = canvas.getContext('2d');
 	Init();
 }
